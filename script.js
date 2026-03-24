583b4aa21876f4d83bad2eee9a738fa8f1485da8
NetNoGames <talwarkhushal825@gmail.com>
2026-03-24 14:25:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+﻿function toggleMenu(){
+  let s=document.getElementById("sidebar");
+  s.style.left = (s.style.left==="0px") ? "-250px" : "0px";
+}
+
+
+function openPanel(img){
+  window.open(img,"_blank");
+}
+
+
+function download(){
+  window.open("https://drive.google.com/","_blank");
+}
