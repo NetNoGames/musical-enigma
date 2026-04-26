7c0bb233624cd65ef894dc1329e95a6302edf18b
NetNoGames <talwarkhushal825@gmail.com>
2026-04-26 16:38:17 +0530

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,69 @@
+﻿// ===== Elements =====
+let sidebar = document.getElementById("sidebar");
+let panel = document.getElementById("panel");
+let panelImg = document.getElementById("panelImg");
+let downloadBtn = document.getElementById("downloadBtn");
+let portalBtn = document.getElementById("portalBtn");
+
+
+// Community buttons
+let communityButtons = document.getElementById("communityButtons");
+let discordBtn = document.getElementById("discordBtn");
+let redditBtn = document.getElementById("redditBtn");
+
+
+// ===== Menu Toggle =====
+function toggleMenu() {
+  sidebar.style.left = sidebar.style.left === "0px" ? "-220px" : "0px";
+}
+
+
+// ===== Panel Open =====
+function openPanel(img) {
+  panel.style.display = "block";
+  panelImg.src = img;
+
+
+  // hide main buttons
+  downloadBtn.style.display = "none";
+  portalBtn.style.display = "none";
+
+
+  // hide community buttons by default
+  if (communityButtons) {
+    communityButtons.style.display = "none";
+  }
+
+
+  // show community buttons only for community panel
+  if (img === "communitypanels.png") {
+    if (communityButtons) {
+      communityButtons.style.display = "block";
+    }
+  }
+
+
+  // close sidebar
+  sidebar.style.left = "-220px";
+}
+
+
+// ===== Download =====
+function downloadLauncher() {
+  alert("Download Start");
+}
+
+
+// ===== Community Links =====
+if (discordBtn) {
+  discordBtn.onclick = () => {
+    window.open("https://discord.gg/PTbxu9nPZq", "_blank");
+  };
+}
+
+
+if (redditBtn) {
+  redditBtn.onclick = () => {
+    window.open("https://www.reddit.com/r/NetNoGamesCommunity/s/TuFLVwt7Ii", "_blank");
+  };
+}
